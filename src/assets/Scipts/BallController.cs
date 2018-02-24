1a2b85414c8f783df0c8640ecd33d091af34daab
Vincent Li <vinli@live.unc.edu>
2018-02-23 20:12:59 -0800

    Commited Demo
    Commited a function demo of EEG / VR Integration

---
+++ src/assets/Scipts/BallController.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpBCI;
+		
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	public static int LEFT_ID = 1;
+	public static int RIGHT_ID = 2;
+
+
+	public float trainingTime = 30;
+
+	public Text leftText;
+	public Text rightText;
+
+	public bool IsTraining { get { return isTraining; } }
+
+	private bool isTraining;
+	private bool trainingLeft;
+	private bool trainingRight;
+
+	private float _leftTrainedTime;
+	private float _rightTrainedTime;
+
+	private float started;
+
+	private bool rightQueued;
+	private bool leftQueued;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		SharpBCIController.BCI.ClearTrainingData ();
+		Debug.Log ("after clearing data");
+		started = Time.time;
+		isTraining = true;
+	}
+
+	private int x;
+	private int y;
+	// FixedUpdate is called once per time frame
+
+	void Update() {
+        if (isTraining) {
+            UpdateTraining();
+        }
+	}
+
+    //void FixedUpdate()
+    //{
+    //    x = x + 1;
+    //    y = y + 1;
+    //    float moveH = Input.GetAxis("Horizontal");
+    //    float moveV = Input.GetAxis("Vertical");
+    //    Debug.Log("Fixed Update Axis: " + Input.GetAxis("Horizontal").ToString());
+    //    if (Input.GetAxis("Horizontal") < 0)
+    //    {
+    //        _leftTrainedTime += Time.deltaTime;
+    //    }
+    //    else if (Input.GetAxis("Horizontal") > 0)
+    //    {
+    //        _rightTrainedTime += Time.deltaTime;
+    //    }
+
+    //    leftText.text = "Left Count: " + _leftTrainedTime.ToString();
+    //    rightText.text = "Right Count: " + _rightTrainedTime.ToString();
+
+    //    //Vector3 movement = new Vector3 (x, y, z);
+    //    Vector3 movement = new Vector3(moveH, 0, moveV);
+    //    rb.AddForce(movement * speed);
+    //}
+
+    SharpBCI.MovingAverageFilter movementFilter = new SharpBCI.MovingAverageFilter(3);
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        if (rightQueued)
+        {
+            var nextH = (float)movementFilter.Filter(speed);
+            rb.AddForce(new Vector3(nextH, 0, moveV));
+            rightQueued = false;
+        }
+        else if (leftQueued)
+        {
+            var nextH = (float)movementFilter.Filter(-speed);
+            rb.AddForce(new Vector3(nextH, 0, moveV));
+            leftQueued = false;
+
+        }
+    }
+
+    void UpdateTraining()
+    {
+        if (trainingLeft)
+        {
+            _leftTrainedTime += Time.deltaTime;
+            leftText.text = "Left Count: " + _leftTrainedTime.ToString();
+
+        }
+        else if (trainingRight)
+        {
+            _rightTrainedTime += Time.deltaTime;
+            rightText.text = "Right Count: " + _rightTrainedTime.ToString();
+        }
+
+        if (isTraining && _rightTrainedTime >= trainingTime && _rightTrainedTime >= trainingTime)
+        {
+            Debug.Log("Done Training");
+            if (trainingRight)
+                SharpBCIController.BCI.StopTraining(RIGHT_ID);
+            if (trainingLeft)
+                SharpBCIController.BCI.StopTraining(LEFT_ID);
+
+            SharpBCIController.BCI.AddTrainedHandler(RIGHT_ID, OnTrainedEvent);
+            SharpBCIController.BCI.AddTrainedHandler(LEFT_ID, OnTrainedEvent);
+            isTraining = false;
+
+            SharpBCIController.BCI.AddTrainedHandler(RIGHT_ID, OnTrainedEvent);
+            SharpBCIController.BCI.AddTrainedHandler(LEFT_ID, OnTrainedEvent);
+        }
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            if (trainingLeft)
+            {
+                SharpBCIController.BCI.StopTraining(LEFT_ID);
+                trainingLeft = false;
+            }
+            if (!trainingRight)
+            {
+                SharpBCIController.BCI.StartTraining(RIGHT_ID);
+                trainingRight = true;
+            }
+
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            if (trainingRight)
+            {
+                SharpBCIController.BCI.StopTraining(RIGHT_ID);
+                trainingRight = false;
+            }
+            else if (!trainingLeft)
+            {
+                SharpBCIController.BCI.StartTraining(LEFT_ID);
+                trainingLeft = true;
+            }
+        } else
+        {
+            if (trainingRight)
+            {
+                SharpBCIController.BCI.StopTraining(RIGHT_ID);
+                trainingRight = false;
+            } else if (trainingLeft)
+            {
+                SharpBCIController.BCI.StopTraining(LEFT_ID);
+                trainingLeft = false;
+            }
+
+        }
+
+    }
+
+
+    void OnTrainedEvent(TrainedEvent evt)
+    {
+        rightQueued = evt.id == RIGHT_ID;
+        leftQueued = evt.id == LEFT_ID;
+
+    }
+}
---
+++ src/assets/Scipts/SharpBCIController.cs
@@ -0,0 +1,184 @@
+﻿using SharpBCI;
+using UnityEngine;
+using System.Diagnostics;
+
+public enum SharpBCIControllerType {
+	MUSE,
+	TONE_GENERATOR,
+	TWO_TONE_GENERATOR,
+	CSV_READER
+}
+
+public class UnityLogger : ILogOutput {
+
+	public void Dispose() {
+		// no cleanup required
+	}
+
+	public void Log(LogLevel level, object message) {
+		switch (level) {
+			case LogLevel.INFO:
+				UnityEngine.Debug.Log(message);
+				break;
+			case LogLevel.WARNING:
+				UnityEngine.Debug.LogWarning(message);
+				break;
+			case LogLevel.ERROR:
+				UnityEngine.Debug.LogError(message);
+				break;
+			default:
+				return;
+		}
+	}
+}
+
+public class SharpBCIController : MonoBehaviour {
+
+	public const int OSC_DATA_PORT = 5000;
+	public const string LOG_NAME = "SharpBCI_log.txt";
+
+	public static SharpBCI.SharpBCI BCI;
+	public static EEGDeviceAdapter adapter;
+
+	public SharpBCIControllerType bciType;
+
+	public EEGDataType dataType;
+
+	public string CSVReadFilePath;
+
+	static SharpBCIController _inst;
+
+	Process museIOProcess;
+
+	bool isTheHighlander = false;
+
+	// Use this for initialization
+	void Awake() {
+		if (_inst != null) {
+			Destroy (gameObject);
+			return;
+		}
+		_inst = this;
+		DontDestroyOnLoad (gameObject);
+		isTheHighlander = true;
+
+		// FileLogger requires actual pathnames not Unity
+		string logName = System.IO.Path.Combine(Application.persistentDataPath.Replace('/', System.IO.Path.DirectorySeparatorChar), LOG_NAME);
+		UnityEngine.Debug.Log("Writing sharpBCI log to: " + logName);
+		// configure logging
+		SharpBCI.Logger.AddLogOutput (new UnityLogger ());
+		//SharpBCI.Logger.AddLogOutput(new FileLogger(logName));
+
+		//EEGDeviceAdapter adapter;
+		if (bciType == SharpBCIControllerType.MUSE) {
+			// start Muse-IO
+			try {
+				museIOProcess = new Process ();
+				museIOProcess.StartInfo.FileName = System.IO.Path.Combine (Application.streamingAssetsPath, "MuseIO", "muse-io");
+				UnityEngine.Debug.Log("filename: " + museIOProcess.StartInfo.FileName);
+				// default is osc.tcp://localhost:5000, but we expect udp
+				museIOProcess.StartInfo.Arguments = "--osc osc.udp://localhost:5000 --device Muse-4B45";
+				museIOProcess.StartInfo.CreateNoWindow = true;
+				museIOProcess.StartInfo.UseShellExecute = false;
+				museIOProcess.Start ();
+				museIOProcess.PriorityClass = ProcessPriorityClass.RealTime;
+			} catch (System.Exception e) {
+				UnityEngine.Debug.LogError ("Could not open muse-io:");
+				UnityEngine.Debug.LogException (e);
+			}
+
+			adapter = new RemoteOSCAdapter (OSC_DATA_PORT);
+		} else if (bciType == SharpBCIControllerType.TONE_GENERATOR) {
+			adapter = new DummyAdapter (new DummyAdapterSignal (new double[] { 
+				// alpha
+				10, 
+				// beta
+				24, 
+				// gamma
+				40, 
+				// delta
+				2, 
+				// theta
+				6,
+				// simulate AC interference
+				60,
+			}, new double[] {
+				512,
+				512,
+				512,
+				512,
+				512,
+				512
+			}), 220, 2);
+		} else if (bciType == SharpBCIControllerType.TWO_TONE_GENERATOR) {
+			var signals = new DummyAdapterSignal[] { 
+				new DummyAdapterSignal (new double[] { 
+					// alpha
+					10, 
+					// beta
+					24, 
+					// gamma
+					40, 
+					// delta
+					2, 
+					// theta
+					6,
+				}, new double[] {
+					512,
+					0,
+					0,
+					0,
+					0
+				}),
+				new DummyAdapterSignal (new double[] { 
+					// alpha
+					10, 
+					// beta
+					24, 
+					// gamma
+					40, 
+					// delta
+					2, 
+					// theta
+					6,
+				}, new double[] {
+					0,
+					512,
+					0,
+					0,
+					0
+				})
+			};
+			adapter = new InstrumentedDummyAdapter (signals, 220, 2);
+		} else if (bciType == SharpBCIControllerType.CSV_READER) {
+			double sampleRate = 220;
+			adapter = new CSVReadAdapter (CSVReadFilePath, sampleRate);
+		} else {
+			throw new System.Exception("Invalid bciType");
+		}
+
+		BCI = new SharpBCIBuilder()
+			.EEGDeviceAdapter(adapter)
+			.PipelineFile(System.IO.Path.Combine(Application.streamingAssetsPath, "default_pipeline.json"))
+			.Build();
+
+		if (bciType != SharpBCIControllerType.CSV_READER) {
+			BCI.LogRawData(dataType);
+		}
+	}
+
+	void OnDestroy() {
+		if (!isTheHighlander)
+			return;
+		
+		if (bciType == SharpBCIControllerType.MUSE) {
+			if (museIOProcess != null && !museIOProcess.HasExited) {
+				museIOProcess.Kill();
+				museIOProcess.WaitForExit();
+			}
+		}
+			
+		BCI.Close();
+		SharpBCI.Logger.Dispose();
+	}
+}
