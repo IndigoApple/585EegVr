26f2277865314d4d1c36671d09e3cf631bb628d4
Vincent Li <vinli@live.unc.edu>
2018-04-19 15:05:28 -0700

    separated trainer file

--- assets/Scripts/MiniGame2/EEGToController.cs
+++ assets/Scripts/MiniGame2/EEGToController.cs
@@ -33,24 +33,15 @@ public class EEGToController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         //add trained events so SharpBCI will call TrainedEvent
-        //SharpBCIController.BCI.AddTrainedHandler(DOWN_ID, TrainedEvent);
-        //SharpBCIController.BCI.AddTrainedHandler(UP_ID, TrainedEvent);
-        SharpBCIController.BCI.ClearTrainingData();
-        started = Time.time;
-        isTraining = true;
-        trainingUp = false;
-        trainingDown = false;
-		EEG.GetComponent<SharpBCIController> ().addBlinkHandler (BlinkEvent);
+        SharpBCIController.BCI.AddTrainedHandler(DOWN_ID, TrainedEvent);
+        SharpBCIController.BCI.AddTrainedHandler(UP_ID, TrainedEvent);
+        EEG.GetComponent<SharpBCIController> ().addBlinkHandler (BlinkEvent);
 		rotation = arrow.GetComponent<arrow_rotation>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (isTraining)
-        {
-            UpdateTraining();
-        } 
-		else if (downQueue) {
+		if (downQueue) {
 			arrow.GetComponent<arrow_rotation>().direction = -1;
 		}
 		else if (upQueue) {
@@ -58,76 +49,6 @@ public class EEGToController : MonoBehaviour {
 		}
 	}
 
-    void UpdateTraining()
-    {
-        if (trainingUp)
-        {
-            _upTrainedTime += Time.deltaTime;
-            Debug.Log("up trained:" + _upTrainedTime.ToString());
-        }
-        else if (trainingDown)
-        {
-            _downTrainedTime += Time.deltaTime;
-            Debug.Log("down trained:" + _downTrainedTime.ToString());
-        }
-
-        if ((isTraining && _upTrainedTime >= trainingTime) && (_downTrainedTime >= trainingTime))
-        {
-            Debug.Log("Done Training");
-            if (trainingDown)
-                SharpBCIController.BCI.StopTraining(DOWN_ID);
-            if (trainingUp)
-                SharpBCIController.BCI.StopTraining(UP_ID);
-
-            SharpBCIController.BCI.AddTrainedHandler(DOWN_ID, TrainedEvent);
-            SharpBCIController.BCI.AddTrainedHandler(UP_ID, TrainedEvent);
-            Debug.Log("Added Trained Handlers");
-            isTraining = false;
-        }
-
-
-        if (Input.GetAxis("Vertical") < 0)
-        {
-            if (trainingUp)
-            {
-                SharpBCIController.BCI.StopTraining(UP_ID);
-                trainingUp = false;
-            }
-            if (!trainingDown)
-            {
-                SharpBCIController.BCI.StartTraining(DOWN_ID);
-                trainingDown = true;
-            }
-        }
-        else if (Input.GetAxis("Vertical") > 0)
-        {
-            if (trainingDown)
-            {
-                SharpBCIController.BCI.StopTraining(DOWN_ID);
-                trainingDown = false;
-            }
-            else if (!trainingUp)
-            {
-                SharpBCIController.BCI.StartTraining(UP_ID);
-                trainingUp = true;
-            }
-        }
-        else
-        {
-            if (trainingDown)
-            {
-                SharpBCIController.BCI.StopTraining(DOWN_ID);
-                trainingDown = false;
-            }
-            else if (trainingUp)
-            {
-                SharpBCIController.BCI.StopTraining(UP_ID);
-                trainingUp = false;
-            }
-
-        }
-
-    }
 
     void TrainedEvent(TrainedEvent evt)
 	{
